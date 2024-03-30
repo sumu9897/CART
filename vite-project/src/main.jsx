fa0b8c7ae6719d27afeb661cee06018b904d5b27
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-03-30 14:07:16 +0600

    CART PROJECT

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
+import './assets/css/Style.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
