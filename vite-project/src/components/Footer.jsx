9898219eb581d9fb380bfd299dea6652bd561e00
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-03-30 20:15:45 +0600

    CART PROJECT

--- vite-project/src/components/Footer.jsx
+++ vite-project/src/components/Footer.jsx
@@ -2,7 +2,13 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
+        <div className="bg-dark container-fluid m-0 p-3">
+            <div className="row">
+                <div className="col-12 text-white">
+                    <p>All Rights Reserved</p>
+                </div>
+
+            </div>
             
         </div>
     );
