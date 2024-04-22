1ee21353025bd2d6289c36bba9688e8b3f9de4ab
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-04-22 09:57:14 +0600

    Product List

--- vite-project/src/components/ProductList.jsx
+++ vite-project/src/components/ProductList.jsx
@@ -1,4 +1,5 @@
 import React, {useEffect} from 'react';
+import axios from "axios";
 import Helper from "../utility/Helper.js";
 import FullScreenLoader from "./FullScreenLoader.jsx";
 
@@ -14,33 +15,41 @@ const ProductList = () => {
 
     const CallProductList=async()=>{
         let res= await axios.get(`${Helper.API_BASE}/product-list`)
-        let productList=res.data['dats'];
+        let productList=res.data['data'];
         setData(productList);
     }
+
     return (
         <div>
-            {data===null?(<FullScreenLoader/>):(
-                <div className="container">
+            {data == null ? (<FullScreenLoader/>) : (
+                <div className="container mt-3">
                     <div className="row">
                         {
-                            data.map((item,i)=>{
-                                return(
+                            data.map((item, i) => {
+                                return (
                                     <div className="col-md-3 p-1">
 
                                         <div className="card p-3">
-                                            <img className="w-100" src={item['image']} alt=""/>
-                                            <h6>{item['title']}</h6>
+
+                                            <img className="w-100" alt="" src={item['image']}/>
+
+                                            <h5> PRICE: $
+                                                {item['discount'] === 0 ? (<span>{item['price']}</span>) : (
+                                                    <span><strike>{<span>{item['price']}</span>}</strike> {
+                                                        <span>{item['discount_price']}</span>}
+                                                </span>
+                                                )}
+                                            </h5>
+
+                                            <p>{item['title']}</p>
                                         </div>
                                     </div>
-
                                 )
-
                             })
                         }
                     </div>
                 </div>
             )}
-
         </div>
     );
 };
