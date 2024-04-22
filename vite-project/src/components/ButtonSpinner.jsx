eafab7cd8ecaebe954a35aee792ddf4de966acb1
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-04-22 13:03:00 +0600

    Button Spinner

--- vite-project/src/components/ButtonSpinner.jsx
+++ vite-project/src/components/ButtonSpinner.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ButtonSpinner = () => {
     return (
-        <div class="spinner-border text-secondary" role="status">
+        <div class="spinner-border spinner-border-sm  text-white" role="status">
             {/*<span className="visually-hidden">Loading...</span>*/}
         </div>
     );
